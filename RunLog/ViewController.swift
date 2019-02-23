5e305a5868b258748114c34ad324d99b70464029
Victor Ruiz <vgruiz@Victors-Mac-mini.local>
2019-02-22 21:48:26 -0800

    Initial commit.

---
+++ RunLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunLog
+//
+//  Created by Victor Ruiz on 2/22/19.
+//  Copyright © 2019 Victor Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
